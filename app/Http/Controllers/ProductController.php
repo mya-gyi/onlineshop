e39de41ff529c026b6e5bbd44ff0bacd85ba2d82
Mya Htut Khaung <myahtutkhaung.ejls@gmail.com>
2024-07-16 15:55:34 +0630

    ProductController update index & create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    // Index Function
+    public function index() {
+        $products = Product::all();
+
+        return view('products.index', compact('products'));
+    }
+
+    public function create() {
+        return view('products.create');
+    }
+
+    
 }
